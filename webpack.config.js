32af700c25ee72599e51dc566c88aa468d75fecf
yuya-yuya <yytyfp5115@icloud.com>
2023-03-01 14:17:59 +0900

    contentBase設定

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: __dirname + "/build"
+  },
+  devServer: {
+    watchContentBase: true,
+    contentBase: path.join(__dirname, 'public')
   }
 }
 
