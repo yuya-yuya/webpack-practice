6a59bb023a6885fc19e4d677c78c1dbdc7d731d3
yuya-yuya <yytyfp5115@icloud.com>
2023-03-01 20:00:37 +0900

    CleanWebpackPluginの導入とpublicのフォルダ構成変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "public"),
-    filename: "main.js"
+    filename: "./javascripts/main.js"
   },
   module: {
     rules: [
@@ -25,11 +26,12 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: './stylesheets/style.css'
     }),
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
   devServer: {
     port: 3000
