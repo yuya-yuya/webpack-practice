dcea3d098f68eda9083478b05b1762644d0aa7a3
yuya-yuya <yytyfp5115@icloud.com>
2023-03-02 13:06:20 +0900

    file-loader追加

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
             loader: "css-loader"
           }
         ]
+      },
+      {
+        test: /\.png|\.jpg/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
