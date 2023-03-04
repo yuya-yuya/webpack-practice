ddd4e91b379a3f423a75aee1086f1f47c029cb73
yuya-yuya <yytyfp5115@icloud.com>
2023-03-05 00:06:13 +0900

    軽微なタイプミス修正

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              source: false
+              sourceMap: false
             }
           },
           {
